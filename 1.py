f1d56d25aa867246fcd765503f6796621d03bc9b
Rdxcj <qoefhsbfnx@gmail.com>
2025-05-07 19:49:25 +0530

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,6 +1,13 @@
 import time
 import html
 import requests
+import os
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException, NoSuchElementException
 from functools import wraps
 from dataclasses import dataclass, field # Use dataclass for structured data
 from lxml import etree
@@ -90,6 +97,58 @@ data = {
     'server_timestamps': 'true',
     'doc_id': '9654017011387330',
 }
+
+def upload(title, description, video):
+    options = Options()
+    options.add_argument("--log-level=3")
+    options.add_argument("--headless")
+    options.add_argument("-profile")
+    options.add_argument("/app/za73pwwo.default")
+    driver = webdriver.Firefox(service=webdriver.firefox.service.Service("/usr/local/bin/geckodriver"), options=options)
+    driver.get("https://www.youtube.com")
+    time.sleep(20)
+    driver.save_screenshot("/app/yt_screenshot/a.png")
+    time.sleep(5)
+    e1 = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "//*[@id=\"buttons\"]/ytd-button-renderer/yt-button-shape/button/yt-touch-feedback-shape/div/div[2]")))
+    e1.click()
+    time.sleep(5)
+    driver.save_screenshot("/app/yt_screenshot/b.png")
+    e2 = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, "ytd-compact-link-renderer:nth-of-type(1) div:nth-of-type(2) > yt-formatted-string:nth-of-type(1)")))
+    e2.click()
+    time.sleep(5)
+    driver.save_screenshot("/app/yt_screenshot/c.png")
+    e3 = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "//*[@id=\"content\"]/input")))
+    e3.send_keys(f"{video}")
+    time.sleep(20)
+    driver.save_screenshot("/app/yt_screenshot/d.png")
+    e4 = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, "div.title ytcp-social-suggestion-input > div")))
+    e4.clear()
+    e4.send_keys(f"""{title}""")
+    time.sleep(5)
+    e5 = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, "div.description ytcp-social-suggestion-input > div")))
+    e5.clear()
+    e5.send_keys(f"""{description}""")
+    time.sleep(5)
+    driver.save_screenshot("/app/yt_screenshot/e.png")
+    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, "ytkc-made-for-kids-select tp-yt-paper-radio-button:nth-of-type(2) > div:nth-of-type(1) > div:nth-of-type(1)"))).click()
+    time.sleep(8)
+    driver.save_screenshot("/app/yt_screenshot/f.png")
+    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "//ytcp-button[@id='next-button']/ytcp-button-shape"))).click()
+    time.sleep(8)
+    driver.save_screenshot("/app/yt_screenshot/g.png")
+    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "//ytcp-button[@id='next-button']/ytcp-button-shape"))).click()
+    time.sleep(8)
+    driver.save_screenshot("/app/yt_screenshot/h.png")
+    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "//ytcp-button[@id='next-button']/ytcp-button-shape"))).click()
+    time.sleep(8)
+    driver.save_screenshot("/app/yt_screenshot/i.png")
+    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, "tp-yt-paper-radio-button:nth-of-type(3) > div:nth-of-type(1) > div:nth-of-type(1)"))).click()
+    time.sleep(8)
+    driver.save_screenshot("/app/yt_screenshot/j.png")
+    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "//ytcp-button[@id='done-button']/ytcp-button-shape"))).click()
+    time.sleep(8)
+    driver.save_screenshot("/app/yt_screenshot/k.png")
+    driver.quit()
 
 # --- 1. Decorator for Timing ---
 def timing_decorator(func):
@@ -274,6 +333,7 @@ class InstagramClipExtractor:
             return StreamUrls()
 
 # --- 4. Main Execution Logic ---
+shitname = []
 @timing_decorator
 def main():
     """Main function to orchestrate the clip extraction process."""
@@ -316,6 +376,7 @@ def main():
                 # --- DOWNLOAD WITH FFMPEG ---
                 if stream_urls.video_url and stream_urls.audio_url:
                     output_file = f"/app/clip_{clip_info.clip_id}.mp4"
+                    shitname.append(output_file)
                     print(f"INFO: Downloading video and audio to '{output_file}'...")
                     ffmpeg_cmd = [
                         "ffmpeg",
@@ -343,6 +404,9 @@ def main():
                 print(f"📝 Title: {clip_info.title}")
                 print(f"🏷️ Description: {clip_info.description}")
                 print(f"🕒 Timestamp: {clip_info.timestamp}")
+                print("💩 Uploading")
+                print(shitname[0])
+                #upload(clip_info.title, clip_info.description, video):
             else:
                 print("❌ Clip node found, but it's missing the MPD manifest.")
         else:
