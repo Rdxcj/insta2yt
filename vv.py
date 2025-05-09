f5d2fed26d56e45996caead36591e6686e418d2a
Rdxcj <qoefhsbfnx@gmail.com>
2025-05-09 08:37:13 +0530

    Update vv.py

--- vv.py
+++ vv.py
@@ -1,11 +1,5 @@
 from seleniumbase import SB
-with SB(uc=True, test=True) as sb:
-  url = "https://gitlab.com/users/sign_in"
+with SB(user_data_dir="/app/chrome/google-chrome", headless2=True, uc=True, ad_block=True) as sb:
+  url = "https://www.youtube.com"
   sb.driver.uc_open_with_reconnect(url, 3)
-  if not sb.is_text_visible("Username", '[for="user_login"]'):
-    sb.driver.uc_open_with_reconnect(url, 4)
-  sb.assert_text("Username", '[for="user_login"]', timeout=3)
-  sb.assert_element('label[for="user_login"]')
-  sb.highlight('button:contains("Sign in")')
-  sb.highlight('h1:contains("GitLab.com")')
-  sb.post_message("SeleniumBase wasn't detected", duration=4)
+  sb.save_screenshot("/app/yt_screenshot/1.png")
