405474735e6cc64115ab8f77f66ccd3d24e3bbe2
Rdxcj <qoefhsbfnx@gmail.com>
2025-05-09 07:22:50 +0530

    Update Uc.py

--- Uc.py
+++ Uc.py
@@ -1,12 +1,18 @@
 from seleniumbase import SB
+import time
 with SB(user_data_dir="/app/chrome/google-chrome", headless2=True, uc=True, ad_block=True, incognito=True) as sb:
     url = "https://www.youtube.com"
-    sb.activate_cdp_mode(url)
-    sb.sleep(15.5)
-    sb.cdp.click("//*[@id=\"buttons\"]/ytd-button-renderer/yt-button-shape/button/yt-touch-feedback-shape/div/div[2]")
-    sb.sleep(5)
-    sb.cdp.save_screenshot("1", "/app/yt_screenshot")
-    sb.cdp.click("ytd-compact-link-renderer:nth-of-type(1) div:nth-of-type(2) > yt-formatted-string:nth-of-type(1)")
-    sb.sleep(10)
-    sb.cdp.save_screenshot("2", "/app/yt_screenshot")
-    sb.sleep(15)
+#    sb.activate_cdp_mode(url)
+#    sb.sleep(15.5)
+#    sb.cdp.click("//*[@id=\"buttons\"]/ytd-button-renderer/yt-button-shape/button/yt-touch-feedback-shape/div/div[2]")
+#    sb.sleep(5)
+#    sb.cdp.save_screenshot("1", "/app/yt_screenshot")
+#    sb.cdp.click("ytd-compact-link-renderer:nth-of-type(1) div:nth-of-type(2) > yt-formatted-string:nth-of-type(1)")
+#    sb.sleep(10)
+#    sb.cdp.save_screenshot("2", "/app/yt_screenshot")
+#    sb.sleep(15)
+    sb.uc_open_with_reconnect(url, 15.111)
+    sb.uc_click("//*[@id=\"buttons\"]/ytd-button-renderer/yt-button-shape/button/yt-touch-feedback-shape/div/div[2]", 4.1)
+    time.sleep(9)
+    sb.uc.click("ytd-compact-link-renderer:nth-of-type(1) div:nth-of-type(2) > yt-formatted-string:nth-of-type(1)", 10)
+    time.sleep(7)
